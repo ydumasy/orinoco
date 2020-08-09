661553432f57b0a8feab317fa9a658ed3f578533
ydumasy <ydumasy@gmail.com>
2020-08-09 19:11:10 +0200

    Création de index.js et product.js et modification de l'affichage de product.html en fonction du produit demandé

---
+++ js/product.js
@@ -0,0 +1,30 @@
+let url = location.search;
+
+const removeChild = () => {
+    let footer = document.querySelector('footer');
+
+    if (url === '?id=teddy') {
+        productTeddy.classList.add("mt-5");
+
+        productCamera.parentNode.removeChild(productCamera);
+        productFurniture.parentNode.removeChild(productFurniture);
+
+        footer.classList.add('fixed-bottom');
+    } else if (url === '?id=camera') {
+        productCamera.classList.add("mt-5");
+
+        productCamera.parentNode.removeChild(productTeddy);
+        productFurniture.parentNode.removeChild(productFurniture);
+
+        footer.classList.add('fixed-bottom');
+    } else if (url === '?id=furniture') {
+        productFurniture.classList.add("mt-5");
+
+        productTeddy.parentNode.removeChild(productTeddy);
+        productCamera.parentNode.removeChild(productCamera);
+
+        footer.classList.add('fixed-bottom');
+    }
+}
+
+removeChild();
