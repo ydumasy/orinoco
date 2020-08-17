53ce29ba0af246f13ae5a60e96ff14d7d024043a
ydumasy <ydumasy@gmail.com>
2020-08-17 18:29:44 +0200

    Affichage des informations spécifiques à chaque produit sur product.html

--- js/product.js
+++ js/product.js
@@ -1,31 +1,14 @@
 let url = location.search;
 
 // Affichage de la page en fonction du produit choisi
-const removeChild = () => {
-    let footer = document.querySelector('footer');
-
-    if (url === '?id=teddy') {
-        productTeddy.classList.add("mt-5");
-
-        productCamera.parentNode.removeChild(productCamera);
-        productFurniture.parentNode.removeChild(productFurniture);
-
-        footer.classList.add('fixed-bottom');
-    } else if (url === '?id=camera') {
-        productCamera.classList.add("mt-5");
-
-        productCamera.parentNode.removeChild(productTeddy);
-        productFurniture.parentNode.removeChild(productFurniture);
-
-        footer.classList.add('fixed-bottom');
+const showProduct = () => {
+    if (url === '?id=teddies') {
+        productTeddies.style.display = 'block';
+    } else if (url === '?id=cameras') {
+        productCameras.style.display = 'block';
     } else if (url === '?id=furniture') {
-        productFurniture.classList.add("mt-5");
-
-        productTeddy.parentNode.removeChild(productTeddy);
-        productCamera.parentNode.removeChild(productCamera);
-
-        footer.classList.add('fixed-bottom');
+        productFurniture.style.display = 'block';
     }
 }
 
-removeChild();
+showProduct();
